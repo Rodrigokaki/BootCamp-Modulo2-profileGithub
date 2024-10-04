f6e93f99493072b35127cb77b1adf12d046246bb
rodrigo kakiuchi de souza <rodrigo.kakiuchi2010@gmail.com>
2024-10-04 10:55:47 -0300

    closes #3 - Tratando erros

--- script.js
+++ script.js
@@ -4,12 +4,30 @@ function search(){
     var url = `https://api.github.com/users/${username}`
 
     $.getJSON(url, (user) =>{
-        
-        document.getElementById("name").innerHTML = user.name
-        document.getElementById("html_url").innerHTML = user.html_url
-        document.getElementById("company").innerHTML = user.company
-        document.getElementById("avatar_url").innerHTML = `<img src="${user.avatar_url}" width="220"
-                    height="220" class="shadow rounded" />`
+        setUserData(user)
+        showError()
+    }).fail( () => {
+        setUserData({})
+        showError("Não encontrado!")
+
     })
 
+}
+
+function setUserData(user){
+    document.getElementById("name").innerHTML = user.name || ""
+    document.getElementById("html_url").innerHTML = user.html_url || ""
+    document.getElementById("company").innerHTML = user.company || ""
+
+    document.getElementById("avatar_url").innerHTML = user.avatar_url ? 
+                                            `<img src="${user.avatar_url}" width="220" height="220" class="shadow rounded" />`
+                                            :
+                                            ""
+}
+
+function showError(msg){
+    document.getElementById("notFoundError").innerHTML = msg ? 
+    `<div class="alert alert-danger" role="alert">${msg}</div>`
+    :
+    ""
 }
